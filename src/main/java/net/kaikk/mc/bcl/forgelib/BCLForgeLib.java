6c882ac9ce2cc872244db76f998f7bb33cc183cb
Avalanche7CZ <avalanche752@gmail.com>
2024-10-20 17:21:16 +0200

    Update BCLForgeLib.java

--- src/main/java/net/kaikk/mc/bcl/forgelib/BCLForgeLib.java
+++ src/main/java/net/kaikk/mc/bcl/forgelib/BCLForgeLib.java
@@ -7,9 +7,10 @@ import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLLoadCompleteEvent;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CompletableFuture;
 import net.minecraft.world.ChunkCoordIntPair;
 import net.minecraft.world.World;
 import net.minecraftforge.common.ForgeChunkManager;
@@ -18,19 +19,18 @@ import net.minecraftforge.common.MinecraftForge;
 @Mod(modid = "BCLForgeLib", version = "1.0", acceptableRemoteVersions = "*")
 public class BCLForgeLib {
     public static final String MODID = "BCLForgeLib";
-
     public static final String VERSION = "1.0";
 
-    private HashMap<String, ForgeChunkManager.Ticket> tickets;
-
-    private HashMap<String, List<ChunkLoader>> chunkLoaders;
+    // Concurrent maps to handle multithreading safely
+    private ConcurrentHashMap<String, ForgeChunkManager.Ticket> tickets;
+    private ConcurrentHashMap<String, List<ChunkLoader>> chunkLoaders;
 
     @Instance("BCLForgeLib")
     private static BCLForgeLib instance;
 
     @EventHandler
     void init(FMLInitializationEvent event) {
-        this.tickets = new HashMap<String, ForgeChunkManager.Ticket>();
+        this.tickets = new ConcurrentHashMap<>();
         instance = this;
         MinecraftForge.EVENT_BUS.register(new EventListener());
     }
@@ -39,15 +39,19 @@ public class BCLForgeLib {
     void onLoadComplete(FMLLoadCompleteEvent evt) {
         instance = this;
         ForgeChunkManager.setForcedChunkLoadingCallback(this, new ChunkLoadingCallback());
+
         try {
             boolean overridesEnabled = getField(ForgeChunkManager.class, "overridesEnabled").getBoolean(null);
-            if (!overridesEnabled)
-                getField(ForgeChunkManager.class, "overridesEnabled").set(null, Boolean.valueOf(true));
-            Map<String, Integer> ticketConstraints = (Map<String, Integer>)getField(ForgeChunkManager.class, "ticketConstraints").get(null);
-            Map<String, Integer> chunkConstraints = (Map<String, Integer>)getField(ForgeChunkManager.class, "chunkConstraints").get(null);
-            ticketConstraints.put("BCLForgeLib", Integer.valueOf(2147483647));
-            chunkConstraints.put("BCLForgeLib", Integer.valueOf(2147483647));
-            this.chunkLoaders = new HashMap<String, List<ChunkLoader>>();
+            if (!overridesEnabled) {
+                getField(ForgeChunkManager.class, "overridesEnabled").set(null, Boolean.TRUE);
+            }
+
+            Map<String, Integer> ticketConstraints = (Map<String, Integer>) getField(ForgeChunkManager.class, "ticketConstraints").get(null);
+            Map<String, Integer> chunkConstraints = (Map<String, Integer>) getField(ForgeChunkManager.class, "chunkConstraints").get(null);
+            ticketConstraints.put("BCLForgeLib", Integer.MAX_VALUE);
+            chunkConstraints.put("BCLForgeLib", Integer.MAX_VALUE);
+
+            this.chunkLoaders = new ConcurrentHashMap<>();
             log("Load complete");
         } catch (Exception e) {
             throw new RuntimeException(e);
@@ -55,42 +59,47 @@ public class BCLForgeLib {
     }
 
     public void addChunkLoader(ChunkLoader chunkLoader) {
-        List<ChunkLoader> chunkLoaders = this.chunkLoaders.get(chunkLoader.getWorldName());
-        if (chunkLoaders == null) {
-            chunkLoaders = new ArrayList<ChunkLoader>();
-            this.chunkLoaders.put(chunkLoader.getWorldName(), chunkLoaders);
-        }
-        log("Added chunk loader at chunk " + chunkLoader.getWorldName() + ":" + chunkLoader.getChunkX() + "," + chunkLoader.getChunkZ() + " (r:" + chunkLoader.range + ")");
+        List<ChunkLoader> chunkLoaders = this.chunkLoaders.computeIfAbsent(chunkLoader.getWorldName(), k -> new ArrayList<>());
         chunkLoaders.add(chunkLoader);
-        for (int i = -chunkLoader.range; i <= chunkLoader.range; i++) {
-            for (int j = -chunkLoader.range; j <= chunkLoader.range; j++)
-                loadChunk(chunkLoader.getWorldName(), chunkLoader.getChunkX() + i, chunkLoader.getChunkZ() + j);
-        }
+        log("Added chunk loader at chunk " + chunkLoader);
+
+        CompletableFuture.runAsync(() -> {
+            for (int i = -chunkLoader.range; i <= chunkLoader.range; i++) {
+                for (int j = -chunkLoader.range; j <= chunkLoader.range; j++) {
+                    loadChunk(chunkLoader.getWorldName(), chunkLoader.getChunkX() + i, chunkLoader.getChunkZ() + j);
+                }
+            }
+        });
     }
 
     public void removeChunkLoader(ChunkLoader chunkLoader) {
         List<ChunkLoader> chunkLoaders = this.chunkLoaders.get(chunkLoader.getWorldName());
         if (chunkLoaders != null) {
-            log("Removed chunk loader at chunk " + chunkLoader.getWorldName() + ":" + chunkLoader.getChunkX() + "," + chunkLoader.getChunkZ() + " (r:" + chunkLoader.range + ")");
             chunkLoaders.remove(chunkLoader);
-            for (int i = -chunkLoader.range; i <= chunkLoader.range; i++) {
-                for (int j = -chunkLoader.range; j <= chunkLoader.range; j++) {
-                    chunkLoaders = getChunkLoadersAt(chunkLoader.getWorldName(), chunkLoader.getChunkX() + i, chunkLoader.getChunkZ() + j);
-                    if (chunkLoaders.isEmpty())
-                        unloadChunk(chunkLoader.getWorldName(), chunkLoader.getChunkX() + i, chunkLoader.getChunkZ() + j);
+            log("Removed chunk loader at chunk " + chunkLoader);
+
+            CompletableFuture.runAsync(() -> {
+                for (int i = -chunkLoader.range; i <= chunkLoader.range; i++) {
+                    for (int j = -chunkLoader.range; j <= chunkLoader.range; j++) {
+                        List<ChunkLoader> remainingLoaders = getChunkLoadersAt(chunkLoader.getWorldName(), chunkLoader.getChunkX() + i, chunkLoader.getChunkZ() + j);
+                        if (remainingLoaders.isEmpty()) {
+                            unloadChunk(chunkLoader.getWorldName(), chunkLoader.getChunkX() + i, chunkLoader.getChunkZ() + j);
+                        }
+                    }
                 }
-            }
+            });
         }
     }
 
     public List<ChunkLoader> getChunkLoadersAt(String worldName, int chunkX, int chunkZ) {
         List<ChunkLoader> chunkLoaders = this.chunkLoaders.get(worldName);
-        List<ChunkLoader> selectedChunkLoaders = new ArrayList<ChunkLoader>();
-        if (chunkLoaders == null || chunkLoaders.isEmpty())
-            return selectedChunkLoaders;
-        for (ChunkLoader chunkLoader : chunkLoaders) {
-            if (chunkLoader.contains(chunkX, chunkZ))
-                selectedChunkLoaders.add(chunkLoader);
+        List<ChunkLoader> selectedChunkLoaders = new ArrayList<>();
+        if (chunkLoaders != null) {
+            for (ChunkLoader chunkLoader : chunkLoaders) {
+                if (chunkLoader.contains(chunkX, chunkZ)) {
+                    selectedChunkLoaders.add(chunkLoader);
+                }
+            }
         }
         return selectedChunkLoaders;
     }
@@ -105,21 +114,20 @@ public class BCLForgeLib {
 
     void loadChunk(String worldName, int chunkX, int chunkZ) {
         World world = Util.getWorld(worldName);
-        ForgeChunkManager.Ticket ticket = this.tickets.get(worldName);
-        if (ticket == null) {
-            ticket = ForgeChunkManager.requestTicket(instance, world, ForgeChunkManager.Type.NORMAL);
-            instance.tickets.put(worldName, ticket);
+        if (world != null && !world.getChunkProvider().chunkExists(chunkX, chunkZ)) {
+            ForgeChunkManager.Ticket ticket = this.tickets.computeIfAbsent(worldName, k -> ForgeChunkManager.requestTicket(instance, world, ForgeChunkManager.Type.NORMAL));
+            ForgeChunkManager.forceChunk(ticket, new ChunkCoordIntPair(chunkX, chunkZ));
         }
-        ForgeChunkManager.forceChunk(ticket, new ChunkCoordIntPair(chunkX, chunkZ));
     }
 
     void unloadChunk(String worldName, int chunkX, int chunkZ) {
         ForgeChunkManager.Ticket ticket = this.tickets.get(worldName);
-        if (ticket != null)
+        if (ticket != null && Util.getWorld(worldName) != null) {
             ForgeChunkManager.unforceChunk(ticket, new ChunkCoordIntPair(chunkX, chunkZ));
+        }
     }
 
-    Map<String, ForgeChunkManager.Ticket> getTickets() {
+    public Map<String, ForgeChunkManager.Ticket> getTickets() {
         return this.tickets;
     }
 
@@ -133,4 +141,3 @@ public class BCLForgeLib {
         return field;
     }
 }
-
